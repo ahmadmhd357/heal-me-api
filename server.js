9c6064f2925544414fe22966d89c8aafe49c9da0
Ahmad Al Hariri <100470190+ahmadmhd357@users.noreply.github.com>
2023-03-18 02:14:06 +0300

    fix

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://heal-me-app.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
